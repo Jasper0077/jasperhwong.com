c23284257fd7e2e70d31c89d6a83d1b4add7bdd9
Jasper Hwong Yu Chen <jasperhwong0077@gmail.com>
2022-08-03 23:33:26 +0800

    💄 add more bio info

--- pages/about.tsx
+++ pages/about.tsx
@@ -7,8 +7,15 @@ const About = () => {
         <h1 className="text-5xl font-bold text-center tracking-tight text-black-400 dark:text-white">
           About Me
         </h1>
-        <div className="mb-8 prose dark:prose-stone leading-6 items-start">
-          <h2 className="text-black-400 dark:text-white">Links</h2>
+        <div className="mb-8 prose dark:prose-dark leading-6 items-start">
+          <h2>Bio</h2>
+          <h3>Job Title</h3>
+          <p>Jasper Hwong - Software Developer</p>
+          <h3>Work Experience</h3>
+          <p>
+            Hi, my name is Jasper Hwong, {"I'm"} a software developer at{" "}
+            <a href="https://unotech.io">Uno Technologies</a> in Singapore.
+          </p>
         </div>
       </div>
     </Container>
