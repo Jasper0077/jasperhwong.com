e28d469fc03d40cd19a21a9a75e91593e6be6154
Jasper Hwong Yu Chen <jasperhwong0077@gmail.com>
2022-08-11 01:03:18 +0800

    use mdx context wrapper and container in root

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,17 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
+import Container from "../components/layouts/Container";
+import { MdxComponentsProvider } from "../contexts/mdxContext";
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
-      <Component {...pageProps} />
+      <MdxComponentsProvider>
+        <Container>
+          <Component {...pageProps} />
+        </Container>
+      </MdxComponentsProvider>
     </ThemeProvider>
   );
 }
