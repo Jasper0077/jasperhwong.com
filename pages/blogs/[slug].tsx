c7387e7a77a36b5e9be5928a51f35ee49f368272
Jasper Hwong Yu Chen <jasperhwong0077@gmail.com>
2022-08-27 11:36:38 +0800

    add components to context

--- pages/blogs/[slug].tsx
+++ pages/blogs/[slug].tsx
@@ -1,10 +1,15 @@
 import { serialize } from "next-mdx-remote/serialize";
 import { GetStaticProps, GetStaticPaths } from "next";
 import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
+import cn from "classnames";
 
 import { Post } from "../../types/Post";
 import { getPost, getAllPosts } from "../../utils/mdx";
 import { ParsedUrlQuery } from "querystring";
+
+//components
+// const Sparkles = dynamic(() => import("../../components/Sparkles"));
+import Sparkles from "../../components/Sparkles";
 
 // props type
 type Props = {
@@ -12,14 +17,23 @@ type Props = {
   frontMatter: Omit<Post, "slug">;
 };
 
+// bootstrapping components
+export const components = {
+  Sparkles
+};
+
 const PostPage: React.FC<Props> = ({ source, frontMatter }: Props) => {
   return (
-    <div className="flex flex-col items-start justify-start mx-auto px-8 my-8">
-      <article>
+    <div className="flex items-start justify-start mx-auto">
+      <article
+        className={cn(
+          "prose prose-stone dark:prose-invert dark:prose-p-gray-200 prose-sm sm:prose-base lg:prose-lg xl:prose-xl 2xl:prose-2xl text-justify max-w-2xl mx-auto mb-16"
+        )}
+      >
         <h1 className="flex w-full max-w-2xl relative text-4xl font-bold mx-auto pt-8 pb-8 sm:pb-16">
           {frontMatter.title}
         </h1>
-        <MDXRemote {...source} />
+        <MDXRemote {...source} components={components} />
       </article>
     </div>
   );
