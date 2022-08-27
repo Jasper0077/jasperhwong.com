de405e8ca1aff5abac80d2ce2ac62b1f7266bfa2
Jasper Hwong Yu Chen <jasperhwong0077@gmail.com>
2022-08-27 11:32:18 +0800

    fix ui display

--- components/layouts/Container.tsx
+++ components/layouts/Container.tsx
@@ -71,7 +71,7 @@ export default function Container({ children, customMeta }: ContainerProps) {
         <meta property="og:site_name" content={meta.name} />
       </Head>
       <div className="flex flex-col justify-center px-8">
-        <nav className="flex items-center justify-between w-full relative max-w-2xl border-gray-200 dark:border-gray-700 mx-auto pt-8 pb-8 sm:pb-16  text-gray-900 bg-gray-50  dark:bg-gray-900 bg-opacity-60 dark:text-gray-100">
+        <nav className="flex items-center sticky justify-between w-full max-w-2xl border-gray-200 dark:border-gray-700 mx-auto pt-4 pb-4 sm:pb-8  text-gray-900 bg-gray-50  dark:bg-gray-900 bg-opacity-60 dark:text-gray-100">
           <a href="#skip" className="skip-nav">
             Skip to content
           </a>
