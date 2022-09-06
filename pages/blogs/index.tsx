70783f360de6c051fa581122e361081d954e0683
Jasper Hwong Yu Chen <jasperhwong0077@gmail.com>
2022-09-06 23:48:54 +0800

    import posts

--- pages/blogs/index.tsx
+++ pages/blogs/index.tsx
@@ -1,16 +1,10 @@
 import type { NextPage, GetStaticProps } from "next";
-import { Post } from "../../types/Post";
-import Link from "next/link";
 import { getAllPosts } from "../../utils/mdx";
 import BlogPostCard from "../../components/blog/BlogPostCard";
-
-// props type
-interface Props {
-  posts: [Post];
-}
+import { posts } from "../../data/posts";
 
 // component render function
-const Blogs: NextPage<Props> = ({ posts }: Props) => {
+const Blogs: NextPage = () => {
   return (
     <div className="flex flex-col justify-center px-8 my-8">
       <h1 className="flex w-full max-w-2xl relative text-4xl font-bold mx-auto pt-8 pb-8 sm:pb-16">
