8dbeb4aea9cfef9df2cf53dfdbeecefd2484aea9
Jasper Hwong Yu Chen <jasperhwong0077@gmail.com>
2022-09-06 23:48:42 +0800

    refactor Post type

--- types/Post.ts
+++ types/Post.ts
@@ -1,16 +1,6 @@
 import { Author } from "./Author";
 
 export interface Post {
-  slug?: string;
-  date?: string;
-  author?: Array<Author>;
-  title?: string;
-  description?: string;
-  tags?: Array<string>;
-  test: string;
-}
-
-export interface PostMeta {
   slug: string;
   title: string;
   author?: Array<Author>;
