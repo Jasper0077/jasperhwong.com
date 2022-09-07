e82aa3f590e9b300b2ac851aa51a0a950f1cb0fd
Jasper Hwong Yu Chen <jasperhwong0077@gmail.com>
2022-09-07 23:33:30 +0800

    🐛 fix: content layout exceed the container

--- pages/blogs/[slug].tsx
+++ pages/blogs/[slug].tsx
@@ -32,29 +32,27 @@ const PostPage: React.FC<Props> = ({
 }: Props) => {
   return (
     <>
-      <div className="flex items-start justify-start mx-auto">
-        <article>
-          <h1 className="flex w-full max-w-2xl relative text-5xl font-bold mx-auto pt-8 pb-8 sm:pb-4 dark:text-white">
-            {frontMatter.title}
-          </h1>
-          <div className="flex flex-col items-start justify-between w-full mt-2 mb-20 md:flex-row md:items-center">
-            <p className="ml-2 text-sm text-orange-600 dark:text-orange-400 italic font-semibold">
-              {"Jasper Hwong / "}
-              {format(parseISO(frontMatter.date), "MMM dd, yyyy")}
-            </p>
-            <p className="mt-2 text-sm text-orange-600 dark:text-orange-400 min-w-32 md:mt-0 italic font-semibold">
-              {wordCount} words {"  •  "} {readingTime}
-            </p>
-          </div>
-          <section
-            className={cn(
-              "prose dark:prose-invert sm:prose-base prose-lg text-justify max-w-2xl mx-8 sm:mx-4 mb-16"
-            )}
-          >
-            <MDXRemote {...source} components={components} />
-          </section>
-        </article>
-      </div>
+      <article className="flex flex-col items-start justify-center w-full max-w-2xl mx-auto mb-16">
+        <h1 className="text-5xl font-bold mx-auto pt-8 pb-8 sm:pb-4 dark:text-white">
+          {frontMatter.title}
+        </h1>
+        <div className="flex flex-col items-start justify-between w-full mt-2 mb-20 md:flex-row md:items-center">
+          <p className="ml-2 text-sm text-orange-600 dark:text-orange-400 italic font-semibold">
+            {"Jasper Hwong / "}
+            {format(parseISO(frontMatter.date), "MMM dd, yyyy")}
+          </p>
+          <p className="mt-2 text-sm text-orange-600 dark:text-orange-400 min-w-32 md:mt-0 italic font-semibold">
+            {wordCount} words {"  •  "} {readingTime}
+          </p>
+        </div>
+        <section
+          className={cn(
+            "prose dark:prose-invert sm:prose-base prose-lg text-justify"
+          )}
+        >
+          <MDXRemote {...source} components={components} />
+        </section>
+      </article>
     </>
   );
 };
