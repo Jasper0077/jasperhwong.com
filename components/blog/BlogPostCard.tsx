021c79f83e28615276ade344374edf0b398e842d
Jasper Hwong Yu Chen <jasperhwong0077@gmail.com>
2022-12-13 23:55:18 +0800

    fix overflow and layout

--- components/blog/BlogPostCard.tsx
+++ components/blog/BlogPostCard.tsx
@@ -10,7 +10,7 @@ const BlogPostCard = (props: CardProps) => {
   return (
     <a
       href={`/blogs/${post.slug}`}
-      className="flex flex-col items-center p-6 max-w-2xl bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+      className="flex flex-col items-start min-h-min p-6 max-w-2xl bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
     >
       <div className="grid grid-cols-3 gap-2">
         <h4 className="mb-1 col-span-3 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
@@ -20,7 +20,7 @@ const BlogPostCard = (props: CardProps) => {
           <p className="font-light text-sm col-span-2 text-gray-400 dark:text-gray-200 my-auto">
             {`Last Edited: ${post.date}`}
           </p>
-          <div className="space-x-2">
+          <div className="space-x-2 overflow-y-hidden">
             <Tag tags={post.tags} />
           </div>
         </div>
