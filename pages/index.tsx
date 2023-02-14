2ab8a7086574680508ecf206b7594d8287eb976f
Jasper Hwong <jasper.hwong@unotech.io>
2023-02-14 23:53:11 +0800

    update homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
+import AnimatedHeading from "components/AnimatedHeading";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <></>;
+  return (
+    <div className="flex flex-col justify-center max-w-2xl mx-auto mb-16 w-full">
+      <AnimatedHeading variant="h1" text="Hi, I am Jasper Hwong." />
+    </div>
+  );
 };
 
 export default Home;
