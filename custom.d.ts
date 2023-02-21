c70150e58708097cd5fa12f26b48d19468c12dbe
Jasper Hwong <jasper.hwong@unotech.io>
2023-02-22 00:43:47 +0800

    update custom slate types

--- custom.d.ts
+++ custom.d.ts
@@ -1,8 +1,15 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { BaseElement } from "slate";
+import { BaseElement, BaseText } from "slate";
 
 declare module "slate" {
   export interface BaseElement {
     type: string;
+  }
+
+  export interface BaseText {
+    bold?: boolean;
+    code?: boolean;
+    italic?: boolean;
+    underline?: boolean;
   }
 }
