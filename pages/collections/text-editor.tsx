82acdf5ead9d54e3a5b70c273df592fd74143deb
Jasper0077 <jasperhwong@hotmail.com>
2023-02-27 00:24:08 +0800

    use wrapper

--- pages/collections/text-editor.tsx
+++ pages/collections/text-editor.tsx
@@ -1,15 +1,16 @@
 import TextEditor from "@ui/commons/TextEditor";
+import Wrapper from "@ui/commons/Wrapper";
 
 const RichTextEditor = () => {
   return (
-    <div className="flex flex-col justify-center max-w-2xl mx-auto mb-16 w-full">
-      <h1 className="text-5xl font-bold text-center tracking-tight text-black-400 dark:text-white">
+    <Wrapper>
+      <h1 className="text-5xl font-bold text-black-400 dark:text-white">
         Slate Text Editor
       </h1>
       <div className="flex mt-4 text-center">
         <TextEditor />
       </div>
-    </div>
+    </Wrapper>
   );
 };
 
