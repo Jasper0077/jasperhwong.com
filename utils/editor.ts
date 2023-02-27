fe1855797668836fa130151fae38e40e53f9ffd9
Jasper0077 <jasperhwong@hotmail.com>
2023-02-28 00:37:08 +0800

    add editor constants

--- utils/editor.ts
+++ utils/editor.ts
@@ -21,4 +21,12 @@ export const initialValue: Element[] = [
   }
 ];
 
-export default initialValue;
+export const PARAGRAPH_STYLES = [
+  "h1",
+  "h2",
+  "h3",
+  "h4",
+  "paragraph",
+  "multiple"
+];
+export const CHARACTER_STYLES = ["bold", "italic", "underline", "code"];
