2e0819c5c484ebff4e1481ed3c4dbb2f875acaea
Jasper0077 <jasperhwong@hotmail.com>
2023-03-01 23:15:12 +0800

    modify state type

--- hooks/useSelectionChange.tsx
+++ hooks/useSelectionChange.tsx
@@ -1,12 +1,12 @@
 import areEqual from "deep-equal";
 import React from "react";
-import { BaseEditor, BaseSelection } from "slate";
+import { BaseEditor, BaseRange } from "slate";
 import { ReactEditor } from "slate-react";
 
 export default function useSelection(editor: BaseEditor & ReactEditor) {
   const [selection, setSelection] = React.useState(editor.selection);
   const setSelectionOptimized = React.useCallback(
-    (newSelection: BaseSelection) => {
+    (newSelection: BaseRange | null) => {
       // don't update the component state if selection hasn't changed.
       if (areEqual(selection, newSelection)) {
         return;
@@ -16,5 +16,5 @@ export default function useSelection(editor: BaseEditor & ReactEditor) {
     [setSelection, selection]
   );
 
-  return [selection, setSelectionOptimized];
+  return { selection, setSelectionOptimized };
 }
