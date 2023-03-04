a5646e17fd1af2a017b3c3ef100f8bff2838eb26
Jasper0077 <jasperhwong@hotmail.com>
2023-03-04 23:00:21 +0800

    add link icon to constants for toolbar

--- components/ui/commons/TextEditor/constants.tsx
+++ components/ui/commons/TextEditor/constants.tsx
@@ -93,5 +93,26 @@ export const CHARACTER_STYLES = [
         <path d="M14 4l-4 16"></path>
       </svg>
     )
+  },
+  {
+    style: "link",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="icon icon-tabler icon-tabler-link"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        strokeWidth="2"
+        stroke="currentColor"
+        fill="none"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path d="M10 14a3.5 3.5 0 0 0 5 0l4 -4a3.5 3.5 0 0 0 -5 -5l-.5 .5"></path>
+        <path d="M14 10a3.5 3.5 0 0 0 -5 0l-4 4a3.5 3.5 0 0 0 5 5l.5 -.5"></path>
+      </svg>
+    )
   }
 ];
