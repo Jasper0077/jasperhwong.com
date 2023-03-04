51f5f98d383580fc8fd77f9cc8b87147ba23753e
Jasper0077 <jasperhwong@hotmail.com>
2023-03-04 23:00:37 +0800

    fix toolbar styling

--- components/ui/commons/TextEditor/Toolbar.tsx
+++ components/ui/commons/TextEditor/Toolbar.tsx
@@ -30,36 +30,40 @@ const Toolbar = ({ editor, selection }: ToolbarProps) => {
   const blockType = getTextBlockStyle(editor);
 
   return (
-    <div className="flex justify-start items-center my-2 space-x-4 z-10">
-      <Dropdown text={blockType ?? "h1"} className="w-24 h-9 z-10">
-        {PARAGRAPH_STYLES.map((s, index) => {
+    <div className="flex items-center justify-between mx-6 my-2 space-x-4">
+      <div>
+        <Dropdown text={blockType ?? "h1"} className="w-24 h-9 z-10">
+          {PARAGRAPH_STYLES.map((s, index) => {
+            return (
+              <Dropdown.Item
+                key={s + index}
+                variant="div"
+                onClick={() => onBlockTypeChange(s)}
+              >
+                {s}
+              </Dropdown.Item>
+            );
+          })}
+        </Dropdown>
+      </div>
+      <div className="flex flex-row space-x-4 justify-center items-center">
+        {CHARACTER_STYLES.map(({ style, icon }, index) => {
           return (
-            <Dropdown.Item
-              key={s + index}
-              variant="div"
-              onClick={() => onBlockTypeChange(s)}
+            <IconButton
+              isActive={getActiveStyles(editor).has(style)}
+              className="rounded w-9 h-9 flex items-center justify-center text-center"
+              key={style + index}
+              handleClick={() => console.log("debug", style)}
+              onMouseDown={(event) => {
+                event.preventDefault();
+                toggleStyle(editor, style);
+              }}
             >
-              {s}
-            </Dropdown.Item>
+              {icon}
+            </IconButton>
           );
         })}
-      </Dropdown>
-      {CHARACTER_STYLES.map(({ style, icon }, index) => {
-        return (
-          <IconButton
-            isActive={getActiveStyles(editor).has(style)}
-            className="rounded w-9 h-9 flex items-center justify-center text-center"
-            key={style + index}
-            handleClick={() => console.log("debug", style)}
-            onMouseDown={(event) => {
-              event.preventDefault();
-              toggleStyle(editor, style);
-            }}
-          >
-            {icon}
-          </IconButton>
-        );
-      })}
+      </div>
     </div>
   );
 };
