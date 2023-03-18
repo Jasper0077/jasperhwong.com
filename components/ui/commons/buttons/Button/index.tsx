6d30e6e043762335ff5c0b168ee495bbe8b423a7
Jasper0077 <jasperhwong@hotmail.com>
2023-03-18 22:50:21 +0800

    fix button text style and sizing

--- components/ui/commons/buttons/Button/index.tsx
+++ components/ui/commons/buttons/Button/index.tsx
@@ -15,7 +15,7 @@ export const buttonStyles = cva("rounded-lg px-2 mx-2", {
   variants: {
     variant: {
       primary:
-        "bg-gray-200 dark:bg-gray-600 text-white border-transparent hover:ring-2 ring-gray-300",
+        "bg-gray-200 dark:bg-gray-600 border-transparent hover:ring-2 ring-gray-300 text-grey-200 dark:text-white",
       secondary: [
         "bg-green-500",
         "text-white",
@@ -24,8 +24,8 @@ export const buttonStyles = cva("rounded-lg px-2 mx-2", {
       ]
     },
     size: {
-      small: ["text-sm", "py-1", "px-2"],
-      medium: ["text-base", "py-2", "px-4"]
+      small: ["text-sm", "py-1", "px-2", "min-w-max", "max-w-24"],
+      medium: ["text-base", "py-2", "px-4", "min-w-max", "max-w-32"]
     }
   },
   defaultVariants: {
@@ -44,7 +44,7 @@ export const Button = ({
   return (
     <button
       type="button"
-      className={cn(buttonStyles({ variant, size }), className)}
+      className={cn(buttonStyles({ variant, size }), className, "w-2")}
       {...props}
     >
       {children}
