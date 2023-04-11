8fa4bd2f61eb4b55465f97300599e7732c7981b5
Jasper Hwong <jasper.hwong@unotech.io>
2023-04-11 12:15:23 +0800

    init breadcrumb component

---
+++ components/ui/commons/Breadcrumb/index.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import styled from "styled-components";
+
+export interface BreadcrumbProps {
+  children: React.ReactNode;
+}
+export interface CrumbProps {
+  href: string;
+  isActive?: boolean;
+  children: React.ReactNode;
+}
+
+const BreadcrumbWrapper = styled.ol`
+  padding: 0;
+  margin: 0;
+  list-style-type: none;
+`;
+
+const CrumbWrapper = styled.li`
+  display: inline;
+  --spacing: 12px;
+  &:not(:first-of-type) {
+    margin-left: var(--spacing);
+
+    &::before {
+      content: "/";
+      opacity: 0.25;
+      margin-right: var(--spacing);
+    }
+  }
+`;
+
+const CrumbLink = styled.a`
+  color: inherit;
+  text-decoration: none;
+
+  &:hover {
+    text-decoration: revert;
+  }
+`;
+
+const Crumb = ({ href, isActive, children }: CrumbProps) => {
+  return (
+    <CrumbWrapper>
+      <CrumbLink href={href} aria-current={isActive ? "page" : undefined}>
+        {children}
+      </CrumbLink>
+    </CrumbWrapper>
+  );
+};
+
+const Breadcrumbs = ({ children }: BreadcrumbProps) => {
+  return (
+    <nav aria-label="Breadcrumb">
+      <BreadcrumbWrapper>{children}</BreadcrumbWrapper>;
+    </nav>
+  );
+};
+
+Breadcrumbs.Crumb = Crumb;
+export default Breadcrumbs;
