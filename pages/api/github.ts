2a7912ee28376bedd80f115d1c7faa707a6ce314
Jasper0077 <jasperhwong@hotmail.com>
2023-07-19 23:55:08 +0800

    check for get method

--- pages/api/github.ts
+++ pages/api/github.ts
@@ -6,37 +6,41 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
-  const { data } = await client.query({
-    query: gql`
-      query {
-        user(login: "Jasper0077") {
-          pinnedItems(first: 6, types: REPOSITORY) {
-            nodes {
-              ... on Repository {
-                name
-                description
-                url
-                repositoryTopics(first: 5) {
-                  nodes {
-                    topic {
-                      name
+  if (req.method === "GET") {
+    const { data } = await client.query({
+      query: gql`
+        query {
+          user(login: "Jasper0077") {
+            pinnedItems(first: 6, types: REPOSITORY) {
+              nodes {
+                ... on Repository {
+                  name
+                  description
+                  url
+                  repositoryTopics(first: 5) {
+                    nodes {
+                      topic {
+                        name
+                      }
+                      url
                     }
-                    url
                   }
-                }
-                languages(first: 3) {
-                  nodes {
-                    name
-                    color
+                  languages(first: 3) {
+                    nodes {
+                      name
+                      color
+                    }
                   }
                 }
               }
             }
           }
         }
-      }
-    `
+      `
+    });
+    res.status(200).json(data);
+  }
+  res.status(405).json({
+    error: "Wrong request method"
   });
-
-  res.status(200).json(data);
 }
