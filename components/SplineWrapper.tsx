4c9288ef39555d56202b61150a302ff90069b3df
Jasper0077 <jasperhwong@hotmail.com>
2023-08-07 09:07:46 +0800

    fix wrong deleted file

--- components/SplineWrapper.tsx
+++
@@ -1,9 +0,0 @@
-import Spline from "@splinetool/react-spline";
-
-const SplineWrapper = () => {
-  return (
-    <Spline scene="https://prod.spline.design/XlPxZ3cTL2KaOani/scene.splinecode" />
-  );
-};
-
-export default SplineWrapper;
