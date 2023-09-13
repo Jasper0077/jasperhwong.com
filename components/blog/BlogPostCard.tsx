067bc3ad2535c0430309ffb1434a53402335932b
Jasper0077 <jasperhwong@hotmail.com>
2023-09-14 01:08:29 +0800

    use next/link for passing in locale to router

--- components/blog/BlogPostCard.tsx
+++ components/blog/BlogPostCard.tsx
@@ -4,6 +4,8 @@ import { Post } from "../../types/Post";
 import Tag from "./Tag";
 import { motion } from "framer-motion";
 import WithAnimation from "components/hoc/WithAnimation";
+import Link from "next/link";
+import cn from "classnames";
 
 export interface CardProps {
   index: number;
@@ -13,9 +15,12 @@ export interface CardProps {
 const BlogPostCard = (props: CardProps) => {
   const { index, post } = props;
   return (
-    <motion.a
-      href={`/blogs/${post.slug}`}
-      className="flex flex-col items-start min-h-min p-6 max-w-2xl bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+    <motion.div
+      className={cn(
+        "flex flex-col items-start min-h-min p-6 max-w-2xl",
+        "bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700",
+        "cursor-pointer"
+      )}
       variants={fadeIn({
         direction: "right",
         type: "spring",
@@ -23,23 +28,25 @@ const BlogPostCard = (props: CardProps) => {
         duration: 0.75
       })}
     >
-      <div className="grid grid-cols-3 gap-2">
-        <h4 className="mb-1 col-span-3 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          {post.title}
-        </h4>
-        <div className="col-span-3 flex flex-col sm:flex-row gap-2">
-          <p className="font-light text-sm col-span-2 text-gray-400 dark:text-gray-200 my-auto">
-            {`Last Edited: ${post.date}`}
-          </p>
-          <div className="space-x-2 overflow-y-hidden">
-            <Tag tags={post.tags} />
+      <Link href={`/blogs/${post.slug}`}>
+        <div className="grid grid-cols-3 gap-2">
+          <h4 className="mb-1 col-span-3 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {post.title}
+          </h4>
+          <div className="col-span-3 flex flex-col sm:flex-row gap-2">
+            <p className="font-light text-sm col-span-2 text-gray-400 dark:text-gray-200 my-auto">
+              {`Last Edited: ${post.date}`}
+            </p>
+            <div className="space-x-2 overflow-y-hidden">
+              <Tag tags={post.tags} />
+            </div>
           </div>
+          <p className="flex font-normal col-span-3 text-gray-700 dark:text-gray-400 my-auto">
+            {post.description}
+          </p>
         </div>
-        <p className="flex font-normal col-span-3 text-gray-700 dark:text-gray-400 my-auto">
-          {post.description}
-        </p>
-      </div>
-    </motion.a>
+      </Link>
+    </motion.div>
   );
 };
 
